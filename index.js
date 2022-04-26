f142a12a730c36d4bb6a4400ebbae7cd33d6f0dd
louise <louise.silva@g.globo>
2022-04-26 16:38:38 -0300

    Retirando comentários

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const rwClient = require("./twitterClient.js")                               // 
 const tweet = async () => {
     try{
         await rwClient.v2.tweet("Bom dia e vamos para mais um dia de luta")
-        // var postTweet = "Bom dia, vamos para a luta";
-        //Agendamento dos tweets
-        // var agenda = schedule.scheduleJob({hour:19, minute:12}, function(){
+        
         
     }catch (e) {
         console.error(e)
