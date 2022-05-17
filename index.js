0ba75be89650b2c6c12d81b9b1531c4ea762185e
louise <louise.silva@g.globo>
2022-05-17 16:48:59 -0300

    feat: adiciona agendamento do tweet

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
                     
 const rwClient = require("./twitterClient.js")                               // Acesso aos tokens            
-// var cronJob = require('cron').CronJob;                                   //Cria agendamentos de tweet
+var cronJob = require('cron').CronJob;                                   //Cria agendamentos de tweet
 const tweet = async () => {
     try{
-        await rwClient.v2.tweet("Bom dia e vamos para mais um dia de luta")
+        cron.schedule("0 11 * * 1", () => {
+            await rwClient.v2.tweet("Bom dia e vamos para mais um dia de luta")
+
+        })
     }catch (e) {
         console.error(e)
     }
